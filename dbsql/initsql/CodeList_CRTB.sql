5afc8941f4cc0195b1d0afe4bc4c774935069c2e
hank <hank85@yeah.net>
2017-12-15 10:29:25 +0800

    20171215 git commit

---
+++ dbsql/initsql/CodeList_CRTB.sql
@@ -0,0 +1,14 @@
+drop table codelist;
+
+create table codelist (
+code char(6) not null ,
+name varchar(16) not null ,
+ipodt date not null,
+totalvol bigint unsigned not null,
+transvol bigint unsigned not null,
+uptime TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+primary key (code)
+)
+ROW_FORMAT=COMPRESSED  
+KEY_BLOCK_SIZE=8;
+;
